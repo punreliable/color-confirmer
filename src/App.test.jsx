99c16bfa3dcf13b910d1ee761b2015a963bc35c4
Brian Smith <briansmith@rocketcentral.com>
2024-07-30 18:36:27 -0400

    First test is passing

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,10 +1,9 @@
 import {render, screen} from '@testing-library/react';
-import {App} from './App';
+import App from './App';
 
 test("App contains correct heading", () => {
 
   render(<App />);
-
-  const heading = screen.getByText(/Color Confirmer/i);
-
+  const heading = screen.getByRole("heading",{ name: /Color Confirmer/i });
+  expect(heading).toBeInTheDocument();
 });
