7c863899cbaa011af37f38c861136c2cf99f1483
Brian Smith <briansmith@rocketcentral.com>
2024-08-01 14:58:51 -0400

    Promo Link added

--- src/App.jsx
+++ src/App.jsx
@@ -4,30 +4,24 @@ import punLogo from '/logo-punreliable.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  function increaseCount(count) {
-    setCount((count) => count + 1);
-  }
 
   const [buttonColor, setButtonColor] = useState('red');
   const newButtonColor = buttonColor === 'red' ? 'blue' : 'red';
+  const viteLink = 'https://vitejs.dev';
+  const promoLink = 'https://www.buymeacoffee.com/punreliable';
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
+        <a href={viteLink} target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
+        <a href={promoLink} target="_blank">
           <img src={punLogo} className="logo punreliable" alt="Punreliable logo" />
         </a>
       </div>
       <h1>Color Confirmer</h1>
       <div className="card">
-        {/* <button className="red" onClick={() => increaseCount(count) }>
-          Change Color to blue
-        </button> */}
         <button className={newButtonColor} onClick={() => setButtonColor(newButtonColor) }>
           Change Color to {newButtonColor}
         </button>
